3594d61d4fda2eba682915e38f770a41a3ec6780
franout <s262620@studenti.polito.it>
2020-07-27 13:24:42 +0200

    removed tmp files

--- scripts/.#simulation.tcl
+++
@@ -1,1 +0,0 @@
-ms20.50@localhost.localdomain.27052:1589040805
--- scripts/simulation.tcl
+++ scripts/simulation.tcl
@@ -1,4 +1,7 @@
 
-
+project compileall
+#it suppress also the warning for the non optimizezd simulation, in questa sim leads to an error
+vsim -novopt -suppress 12110 work.tb_memories # for suppressiang the error on -novopt
+vsim -vopt work.{} # tes bench  name
 add waves *
-run 1000 ns 
+run 1000ns 
