10d6d5cc1fe339e8d4ad20a99c702532e9e8c189
franout <s262620@studenti.polito.it>
2020-08-21 22:52:55 +0200

    added dram python generator script for random init vales

---
+++ hardware/dlx/test_bench/memories/generate_random_values_dram.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import numpy as np
+
+address_size=19 # for 32 it is too long 
+
+f=open("dram.txt","w")
+for i in range(2**address_size):
+	print(i)
+	f.write(hex(np.random.randint(0,2**32-1,dtype=np.uint32))[2:].zfill(8))
+	f.write("\n")
+f.close()
+exit()
